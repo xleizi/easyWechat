3a7d3cd25075a0b570c053996841247252eaa0d5
xleizi <cuilei798@qq.com>
2024-06-18 14:58:39 +0800

    0.1.0

--- R/wechat.R
+++ R/wechat.R
@@ -1,27 +1,24 @@
 #' WechatHelper
 #'
-#' @param userid 请关注公众号"生信通知助手"，回复id，扫描二维码获取userid
-#' @param msg 需要发生的通知内容
-#' @param status 当前数据状态
-#' @param name 项目名称
-#' @param number 通知编号
+#' @param userid Please follow the official WeChat account "shengxintongzhizhushou", reply with id, and scan the QR code to obtain the userid. View "http://bj.s1f.ren/gzh/home.html" for more information.
+#' @param msg  Content of the notification to be sent.
+#' @param status Current data status.
+#' @param name Project name.
+#' @param number Notification number.
 #'
-#' @return none
+#' @return response of the request
 #' @export
 #'
 #' @examples
+#' WechatMsg(userid = "123", msg = "SaveRds Success!")
 WechatMsg <- function(userid, msg, status = NULL, name = NULL, number = NULL) {
-  library(httr)
-
   base_url <- "http://bj.s1f.ren/gzh/sendMsg"
 
-  # 构建查询参数
   query_params <- list(
     userid = userid,
     text = msg
   )
 
-  # 添加可选参数（如果提供）
   if (!is.null(status)) {
     query_params$status <- status
   }
@@ -32,30 +29,30 @@ WechatMsg <- function(userid, msg, status = NULL, name = NULL, number = NULL) {
     query_params$number <- number
   }
 
-  # 发送GET请求
-  response <- GET(base_url, query = query_params)
+  response <- httr::GET(base_url, query = query_params)
 
-  # 检查响应状态
-  if (status_code(response) == 200) {
-    content(response, "text", encoding = "UTF-8")
+  if (httr::status_code(response) == 200) {
+    httr::content(response, "text", encoding = "UTF-8")
   } else {
-    stop("Request failed with status code: ", status_code(response))
+    stop("Request failed with status code: ", httr::status_code(response))
   }
+  return(response)
 }
 
 
-#' WechatRobot_R
-
-#' @param key 机器人的key
-#' @param msg 文本信息
-#' @param qyapi
-#' @param msgtype
+#' WechatRobot
 #'
-#' @return
+#' @param key The key for the WeChat Work robot.
+#' @param msg Text message.
+#' @param qyapi WeChat Work API.
+#' @param msgtype Type of text message being sent.
+#'
+#' @return response of the request
 #' @export
 #'
 #' @examples
-WechatRobot_R <- function(key, msg = "", qyapi = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=", msgtype = "text") {
+#' WechatRobot(key = "79bdbb23-7f7e-4fb6-b669-97f7e744755e", msg = "SaveRds Success!")
+WechatRobot <- function(key, msg = "", qyapi = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=", msgtype = "text") {
   headers <- c("Content-Type" = "application/json")
   url <- paste0(qyapi, key)
   mypayload <- list(
@@ -69,22 +66,4 @@ WechatRobot_R <- function(key, msg = "", qyapi = "https://qyapi.weixin.qq.com/cg
   return(response)
 }
 
-#' WechatRobot
-#'
-#' @param key
-#' @param msg
-#' @param meth
-#'
-#' @return
-#' @export
-#'
-#' @examples
-WechatRobot <- function(key = "", msg = "", meth = "R") {
-  if (meth == "python") {
-    reticulate::py_run_string("from easyBio import WechatRobot")
-    reticulate::py_run_string(sprintf("wcr = WechatRobot(key = '%s')", key))
-    reticulate::py_run_string(sprintf("wcr.send_mes(mes_info = '%s')", msg))
-  } else if (meth == "R") {
-    WechatRobot_R(key = key, msg = msg)
-  }
-}
+
